9d56f01477407f8d300314afb7cba02c0a7d5303
Wilder-wfo <wiflores24@gmail.com>
2023-04-09 01:13:55 -0400

    Add Input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled";
+
+const StyledInput = styled.input`
+  border-radius: 6px;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.05);
+  padding: 8px 12px;
+  background: #ffffff;
+  width: 100%;
+`;
+function Input({
+  id,
+  name,
+  type = "text",
+  value,
+  onChange,
+  placeholder,
+  label,
+}) {
+  return (
+    <div style={{ width: "100%" }}>
+      {label && <label htmlFor={id || name}>{label}</label>}
+      <StyledInput
+        type={type}
+        name={name}
+        id={id || name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+export default Input;
